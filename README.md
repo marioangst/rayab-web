b20cde3a1ff726210a5fca8c9bf491008343ad49
Mario Angst <mario.angst@gmail.com>
2024-12-08 22:03:55 +0100

    feat: first working version of shiny app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rayab-web
-🤖 🧶 Web application for the rayab R package to create images ready to be knitted using the AYAB software 🤖 🧶 
+
+🤖 🧶 Web interface using the rayab R package to create images ready to be knitted using the AYAB software 🤖 🧶 
+
+The web application uses shinylive to convert a shiny app developped in /app to serve from a static server.
---
+++ app/app.R
@@ -0,0 +1,121 @@
+library(shiny)
+library(rayab)
+library(magick)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("🤖 🧶"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      # Input: Text -----
+      h2("Knit text"),
+      textInput(inputId = "text",
+                label = "Enter text",
+                value = "AYAB"),
+      numericInput(inputId = "textWidth",
+                   value = 200,
+                   label = "desired text image width"),
+      numericInput(inputId = "textHeight",
+                   value = 100,
+                   label = "desired text image height"),
+      downloadButton("downloadTextImage",
+                     "Download text image"),
+      h2("Upload and convert image"),
+      fileInput("upload", "Upload image to convert for knitting",
+                accept = c('image/png', 'image/jpeg')),
+      numericInput(inputId = "convertWidth",
+                   value = 200,
+                   label = "desired converted image width"),
+      numericInput(inputId = "convertHeight",
+                   value = 100,
+                   label = "desired converted image height"),
+      downloadButton("downloadConvertImage",
+                     "Download converted image"),
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: Image ----
+      imageOutput(outputId = "textImage"),
+      imageOutput(outputId = "convertImage")
+
+    )
+  )
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output, session) {
+
+  # text conversion section -----------
+
+  textImage <- reactive({
+    # write image to tmpfile to send out again
+    tmpfile <-
+      rayab::text_to_ayab(input_text = input$text,
+                          width = input$textWidth,
+                          height = input$textHeight) |>
+      image_write(tempfile(fileext='png'), format = 'png')
+    list(src = tmpfile, contentType = "image/png")
+  })
+
+  output$textImage <- renderImage({
+    textImage()
+  },deleteFile = FALSE)
+
+  output$downloadTextImage <- downloadHandler(
+    filename = function() {
+      "knit_me.png"
+    },
+    content = function(file) {
+      img <- textImage()$src
+      file.copy(img, file)
+    }
+  )
+
+  # image conversion section ----------
+
+  # Start with placeholder imae
+  uploadedImage <- magick::logo
+
+  # When uploading new image
+  observeEvent(input$upload, {
+    if (length(input$upload$datapath))
+      uploadedImage <<- image_read(input$upload$datapath)
+  })
+
+  convertImage <- reactive({
+    # write image to tmpfile to send out again
+    tmpfile <-
+      uploadedImage |>
+      rayab::magick_to_ayab(
+        width = input$convertWidth,
+        height = input$convertHeight) |>
+      image_write(tempfile(fileext='png'), format = 'png')
+    list(src = tmpfile, contentType = "image/png")
+  })
+
+  output$convertImage <- renderImage({
+    convertImage()
+  },deleteFile = FALSE)
+
+  output$downloadConvertImage <- downloadHandler(
+    filename = function() {
+      "knit_me.png"
+    },
+    content = function(file) {
+      img <- convertImage()$src
+      file.copy(img, file)
+    }
+  )
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
